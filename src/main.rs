7bbfb758212a30fef2338bb4a0571d0751fea594
Stephen Herbein <stephen272@gmail.com>
2020-11-23 15:18:30 -0800

    main: lookup valid KVS values for new Flux instance
    Problem: "thing" and "other_thing" do not exist in Flux's KVS by
    default, meaning that the default built binary always panics with
    an OS error of FileNotFound.
    
    Solution: Switch to looking up values in the KVS that exist within
    a fresh Flux instance by default.

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,15 @@ fn main() -> Result<(), Box<dyn Error>> {
     eprintln!("got a handle!");
     eprintln!("Hello, world! size:{:?}", h.attr_get("size")?);
     let mut composite = h
-        .kvs_lookup(0, "thing")?
+        .kvs_lookup(0, "resource.eventlog")?
         .and_then(|fi| {
             eprintln!("kvs result:{:?}", fi.lookup_get()?);
-            h.kvs_lookup(0, "other_thing")
-        })?
-        .and_then(|fi| {
-            eprintln!("kvs result2:{:?}", fi.lookup_get()?);
-            h.kvs_lookup(0, "other_thing")
+            h.kvs_lookup(0, "resource.hwloc.by_rank")
         })?
         .then(|f| {
             // f is strongly typed with kvs future methods
-            println!("kvs result3:{:?}", f.lookup_get().unwrap());
+            println!("kvs result2:{:?}", f.lookup_get().unwrap());
+            flux_rs::reactor_stop(&mut h);
         })?;
     composite.wait_for(-1.0)?;
     unsafe {
